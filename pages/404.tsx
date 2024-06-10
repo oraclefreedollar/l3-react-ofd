9067d0cde19573fdf46cc23c77652c25f04a5513
jihadhasanjoy <jihadhasanjoy@outlook.com>
2024-06-10 13:53:33 +0600

    chore: update ofb icon

--- pages/404.tsx
+++ pages/404.tsx
@@ -15,7 +15,7 @@ export default function Custom404() {
 			<div className="flex flex-col items-center justify-center w-full text-center" style={{ height: "60vh" }}>
 				<h1 className="text-right text-4xl font-bold">
 					<picture>
-						<img src="/assets/logo.png" alt="logo" className="h-20" />
+						<img src="/assets/logo.svg" alt="logo" className="h-20" />
 					</picture>
 				</h1>
 				<h1 className="text-4xl font-bold mt-10">You seem to be in the wrong place</h1>
