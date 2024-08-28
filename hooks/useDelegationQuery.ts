edf6bdf81c51d2ab7591cee0709aa7bba4ddd66c
Shekh-Shihab <shekh.shihab@selisegroup.com>
2024-08-28 16:06:51 +0600

    fix-delegated to you issue

--- hooks/useDelegationQuery.ts
+++ hooks/useDelegationQuery.ts
@@ -2,26 +2,26 @@ import { gql, useQuery } from "@apollo/client";
 import { zeroAddress } from "viem";
 
 export const useDelegationQuery = (owner: string) => {
-  const { data, loading } = useQuery(
-    gql`query {
-      delegation(id: "${owner.toLowerCase()}") {
+	const { data, loading } = useQuery(
+		gql`query {
+      delegation(id: "${owner}") {
         id
         owner
         delegatedTo
         pureDelegatedFrom
       }
     }`
-  );
+	);
+	console.log({ data });
+	if (loading || !data || !data.delegation) {
+		return {
+			delegatedTo: zeroAddress,
+			pureDelegatedFrom: [],
+		};
+	}
 
-  if (loading || !data || !data.delegation) {
-    return {
-      delegatedTo: zeroAddress,
-      pureDelegatedFrom: [],
-    };
-  }
-
-  return {
-    delegatedTo: data.delegation.delegatedTo || zeroAddress,
-    pureDelegatedFrom: data.delegation.pureDelegatedFrom,
-  };
+	return {
+		delegatedTo: data.delegation.delegatedTo || zeroAddress,
+		pureDelegatedFrom: data.delegation.pureDelegatedFrom,
+	};
 };
