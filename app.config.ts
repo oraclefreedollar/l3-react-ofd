ebef17be74ee4f62c6e7f684bfdd23d4ea0f732f
Shekh-Shihab <shekh.shihab@selisegroup.com>
2024-09-08 12:43:33 +0600

    env variable test1

--- app.config.ts
+++ app.config.ts
@@ -27,7 +27,7 @@ export const COINGECKO_API_KEY = envConfig.COINGECKO_API_KEY; // demo key @samcl
 // WAGMI CONFIG
 // FIXME: move to env or white list domain
 export const WAGMI_PROJECT_ID = envConfig.WAGMI_PROJECT_ID;
-export const WAGMI_CHAINS = envConfig.ENV == ("dev" || "local") ? [bscTestnet] : null;
+export const WAGMI_CHAINS = envConfig.ENV == ("dev" || "local") ? [bscTestnet] : [null];
 export const WAGMI_METADATA = {
 	name: "OracleFreeDollar",
 	description: "OracleFreeDollar Frontend Application",
