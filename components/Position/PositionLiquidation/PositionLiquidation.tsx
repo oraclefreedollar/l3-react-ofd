5b21a00af322d10fe7fb70862e44aad34ad37ed4
Silas <siou10@gmail.com>
2024-12-28 21:39:10 +0800

    fix build

--- components/Position/PositionLiquidation/PositionLiquidation.tsx
+++ components/Position/PositionLiquidation/PositionLiquidation.tsx
@@ -20,16 +20,16 @@ const PositionLiquidation: React.FC = () => {
 		<div className="bg-gradient-to-br from-purple-900/90 to-slate-900/95 backdrop-blur-md rounded-xl p-8 flex flex-col border border-purple-500/50 gap-y-4">
 			<div className="text-lg font-bold text-center mt-3">Liquidation</div>
 			<TokenInput
-				label="Liquidation Price"
 				balanceLabel="Pick"
-				symbol="OFD"
-				error={errors['liqPrice']}
 				digit={36n - collTokenData.decimals}
+				error={errors['liqPrice']}
 				hideMaxLabel={minCollAmount == 0n}
+				label="Liquidation Price"
 				max={minCollAmount == 0n ? 0n : (5000n * 10n ** 36n + minCollAmount - 1n) / minCollAmount}
-				value={liqPrice.toString()}
 				onChange={(value) => onChangeValue('liqPrice', value)}
 				placeholder="Price"
+				symbol="OFD"
+				value={liqPrice.toString()}
 			/>
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-2">
 				<NormalInput
