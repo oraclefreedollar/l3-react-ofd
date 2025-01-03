6f6b0b47b5ba579b6c6093d79ccc77969762bbdd
NightHarbor <nightharbor91@gmail.com>
2025-01-03 12:37:52 +0100

    New logo assets (#110)

--- pages/index.tsx
+++ pages/index.tsx
@@ -151,7 +151,7 @@ export default function Home() {
 						viewport={{ once: true }}
 						whileInView={{ opacity: 1, scale: 1 }}
 					>
-						<img alt="Oracle Free Dollar Logo" className="w-3/5 max-w-md" src="/assets/oracle-free-dollar-logo-square.svg" />
+						<img alt="Oracle Free Dollar Logo" className="w-3/5 max-w-md" src="/assets/ofd-white.png" />
 					</motion.div>
 				</div>
 
