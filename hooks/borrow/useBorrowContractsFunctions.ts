4d3e349038a266f71544ce6cd67f0a38e090e142
NightHarbor <nightharbor91@gmail.com>
2025-01-15 09:48:57 +0100

    Fix borrow page approve and displayed infos

---
+++ hooks/borrow/useBorrowContractsFunctions.ts
@@ -0,0 +1,83 @@
+import { useMemo } from 'react'
+import { formatBigInt, shortenAddress, toTimestamp } from 'utils'
+import { ABIS, ADDRESS } from 'contracts'
+import { useWriteContractWithToast } from 'hooks'
+import { erc20Abi, formatUnits } from 'viem'
+import { useChainId } from 'wagmi'
+import { PositionStats } from 'meta/positions'
+
+type Props = {
+	amount: bigint
+	expirationDate: Date
+	position: `0x${string}`
+	positionStats: PositionStats
+	requiredColl: bigint
+}
+
+type Returned = {
+	handleApprove: () => Promise<void>
+	handleClone: () => Promise<void>
+	isApproving: boolean
+	isCloning: boolean
+}
+
+export const useBorrowContractsFunctions = (props: Props): Returned => {
+	const { amount, expirationDate, position, positionStats, requiredColl } = props
+
+	const chainId = useChainId()
+	const expirationTime = toTimestamp(expirationDate)
+
+	const approveToastMessageRows = useMemo(
+		() => [
+			{
+				title: 'Amount:',
+				value: `${formatUnits(requiredColl, positionStats.collateralDecimal)} ${positionStats.collateralSymbol}`,
+			},
+			{
+				title: 'Spender: ',
+				value: shortenAddress(ADDRESS[chainId].mintingHub),
+			},
+		],
+		[chainId, positionStats.collateralDecimal, positionStats.collateralSymbol, requiredColl]
+	)
+
+	const { loading: isApproving, writeFunction: handleApprove } = useWriteContractWithToast({
+		contractParams: {
+			address: positionStats.collateral!,
+			abi: erc20Abi,
+			functionName: 'approve',
+			args: [ADDRESS[chainId].mintingHub, requiredColl],
+		},
+		toastPending: { title: `Approving ${positionStats.collateralSymbol}`, rows: approveToastMessageRows },
+		toastSuccess: { title: `Successfully Approved ${positionStats.collateralSymbol}`, rows: approveToastMessageRows },
+		refetchFunctions: [positionStats.refetch],
+	})
+
+	const cloneToastMessageRows = useMemo(
+		() => [
+			{
+				title: `Amount: `,
+				value: formatBigInt(amount) + ' OFD',
+			},
+			{
+				title: `Collateral: `,
+				value: formatBigInt(requiredColl, positionStats.collateralDecimal) + ' ' + positionStats.collateralSymbol,
+			},
+		],
+		[amount, positionStats.collateralDecimal, positionStats.collateralSymbol, requiredColl]
+	)
+
+	const { loading: isCloning, writeFunction: handleClone } = useWriteContractWithToast({
+		contractParams: {
+			address: ADDRESS[chainId].mintingHub,
+			abi: ABIS.MintingHubABI,
+			functionName: 'clone',
+			args: [position, requiredColl, amount, expirationTime],
+		},
+		toastPending: { title: `Minting OFD`, rows: cloneToastMessageRows },
+		toastSuccess: { title: `Successfully Minted OFD`, rows: cloneToastMessageRows },
+		refetchFunctions: [positionStats.refetch],
+	})
+
+	return { handleApprove, handleClone, isApproving, isCloning }
+}
--- meta/positions/index.ts
+++ meta/positions/index.ts
@@ -11,3 +11,32 @@ export type PositionCollateralTokenData = {
 	symbol: string
 	totalSupply: bigint
 }
+
+export type PositionStats = {
+	annualInterestPPM: bigint
+	available: bigint
+	challengePeriod: bigint
+	closed: boolean
+	collateral: `0x${string}` | undefined
+	collateralAllowance: bigint
+	collateralBal: bigint
+	collateralDecimal: number
+	collateralPosAllowance: bigint
+	collateralSymbol: string
+	collateralUserBal: bigint
+	cooldown: bigint
+	denied: boolean
+	expiration: bigint
+	isSuccess: boolean
+	limit: bigint
+	limitForClones: bigint
+	liqPrice: bigint
+	minimumCollateral: bigint
+	minted: bigint
+	mintingFee: bigint
+	ofdAllowance: bigint
+	ofdBalance: bigint
+	owner: `0x${string}`
+	refetch: any
+	reserveContribution: bigint
+}
