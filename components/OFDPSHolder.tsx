099b8820371d343d1864297711457758e64fce74
NightHarbor <nightharbor91@gmail.com>
2025-01-30 16:28:43 +0100

    OFDPS Holder + code cleanup

--- components/OFDPSHolder.tsx
+++ components/OFDPSHolder.tsx
@@ -2,6 +2,8 @@ import { Address } from 'viem'
 import { formatBigInt } from 'utils/format'
 import AddressLabel from './AddressLabel'
 import AppBox from './AppBox'
+import { CoinTicker } from 'meta/coins'
+import { useTranslation } from 'react-i18next'
 
 interface Props {
 	holder: Address
@@ -11,13 +13,18 @@ interface Props {
 }
 
 export default function OFDPSHolder({ holder, ofdps, votingPower, totalVotingPower }: Props) {
+	const { t } = useTranslation()
 	return (
 		<AppBox className="hover:bg-slate-700 duration-300 flex grid grid-cols-1 sm:grid-cols-3">
 			<div className="col-span-1">
 				<AddressLabel address={holder} showCopy showLink />
 			</div>
-			<div className="col-span-1 sm:text-center">{formatBigInt(ofdps)} OFDPS</div>
-			<div className="col-span-1 sm:text-right">{formatBigInt((votingPower * 10000n) / totalVotingPower, 2)} % Votes</div>
+			<div className="col-span-1 sm:text-center">
+				{formatBigInt(ofdps)} {CoinTicker.OFDPS}
+			</div>
+			<div className="col-span-1 sm:text-right">
+				{formatBigInt((votingPower * 10000n) / totalVotingPower, 2)} {t('common:equity:votes')}
+			</div>
 		</AppBox>
 	)
 }
