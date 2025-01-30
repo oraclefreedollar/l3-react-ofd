350957562cb55bab7bc313e956bbd7eb4b5fda2a
Silas <siou10@gmail.com>
2025-01-30 15:35:56 +0100

    new borrow page

--- pages/collateral.tsx
+++ pages/collateral.tsx
@@ -1,17 +1,88 @@
 import Head from 'next/head'
-import OverviewTVL from 'components/OverviewTVL'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useSelector } from 'react-redux'
 import AppPageHeader from 'components/AppPageHeader'
 import { envConfig } from 'app.env.config'
+import Table from 'components/Table'
+import TableBody from 'components/Table/TableBody'
+import TableHeader from 'components/Table/TableHead'
+import TableRowEmpty from 'components/Table/TableRowEmpty'
+import BorrowPositionRow from 'components/Borrow/BorrowPositionRow'
+import { RootState } from 'redux/redux.store'
+import { PositionQuery } from 'redux/slices/positions.types'
 
 export default function Overview() {
-	return (
-		<div>
-			<Head>
-				<title>{envConfig.AppName} - Collateral</title>
-			</Head>
+    const { openPositionsByCollateral } = useSelector((state: RootState) => state.positions)
+    const [list, setList] = useState<PositionQuery[]>([]);
 
-			<AppPageHeader title="Positions By Collateral" />
-			<OverviewTVL />
-		</div>
-	)
+    const openPositions = useMemo(() => 
+        openPositionsByCollateral.flat(1),
+        [openPositionsByCollateral]
+    );
+
+    const makeUnique = useCallback((positions: PositionQuery[]) => {
+        const highestMaturityMap = new Map();
+        positions.forEach((pos) => {
+            const key = pos.original + "-" + pos.price;
+            if (!highestMaturityMap.has(key)) {
+                highestMaturityMap.set(key, pos);
+            } else {
+                const highest = highestMaturityMap.get(key);
+                if (pos.expiration > highest.expiration) {
+                    highestMaturityMap.set(key, pos);
+                }
+            }
+        });
+        return Array.from(highestMaturityMap.values());
+    }, []);
+
+    const matchingPositions = useMemo(() => {
+        const filteredPositions = openPositions.filter((position) => {
+            if (position.closed || position.denied) {
+                return false;
+            } else if (position.cooldown * 1000 > Date.now()) {
+                return false; // under cooldown
+            } else if (BigInt(position.isOriginal ? position.availableForClones : position.availableForMinting) === 0n) {
+                return false;
+            } else {
+                return true;
+            }
+        });
+        
+        return makeUnique(filteredPositions);
+    }, [openPositions, makeUnique]);
+
+    useEffect(() => {
+        setList(matchingPositions);
+    }, [matchingPositions]);
+
+    const headers: string[] = [
+        'Collateral',
+        'Loan-to-Value',
+        'Effective Interest',
+        'Liquidation Price',
+        'Maturity',
+        'Action',
+    ];
+
+    return (
+        <div>
+            <Head>
+                <title>{envConfig.AppName} - Collateral</title>
+            </Head>
+
+            <AppPageHeader title="Mint fresh OFD" />
+            <Table>
+                <TableHeader headers={headers} />
+                <TableBody>
+                    {list.length === 0 ? (
+                        <TableRowEmpty>{'There are no savings yet.'}</TableRowEmpty>
+                    ) : (
+                        list.map((r) => <BorrowPositionRow item={r} key={r.position} />)
+                    )}
+                </TableBody>
+				
+            </Table>
+        </div>
+    );
 }
