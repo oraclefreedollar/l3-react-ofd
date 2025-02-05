acd66cd008bce19cf8a78ed4c4e8511050fb5173
NightHarbor <nightharbor91@gmail.com>
2025-02-05 18:09:16 +0100

    Update configuration setup

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,13 +1,29 @@
+const HttpBackend = require('i18next-http-backend/cjs')
+const ChainedBackend = require('i18next-chained-backend').default
+const LocalStorageBackend = require('i18next-localstorage-backend').default
+
+const isBrowser = typeof window !== 'undefined'
+const isDev = process.env.NEXT_PUBLIC_ENV === 'dev'
+
 /** @type {import('next-i18next').UserConfig} */
-const nextI18nextConfig = {
+module.exports = {
+	// It should config backend, use, partialBundledLanguages in case you want to translate for auto static page
+	backend: {
+		backendOptions: [{ expirationTime: isDev ? 60 * 1000 : 60 * 60 * 1000 }, {}], // 1 hour
+		backends: isBrowser ? [LocalStorageBackend, HttpBackend] : [],
+	},
+	// https://www.i18next.com/overview/configuration-options#logging
+	debug: isDev,
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['en'],
 	},
-	supportedLngs: ['en'],
-	reloadOnPrerender: true,
-	localeDetection: true,
-	fallbackLng: 'en',
+	initAsync: false,
+	/** To avoid issues when deploying to some paas (vercel...) */
+	localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
+	ns: ['common', 'home', 'swap'],
+	partialBundledLanguages: isBrowser,
+	reloadOnPrerender: isDev,
+	use: isBrowser ? [ChainedBackend] : [],
+	react: { useSuspense: false },
 }
-
-module.exports = nextI18nextConfig
