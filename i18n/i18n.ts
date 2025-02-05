0cf21769435b8e6338be21722ebe40fcfd9a9bcc
NightHarbor <nightharbor91@gmail.com>
2025-02-05 17:37:36 +0100

    Fix build error

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -2,7 +2,7 @@ import { initReactI18next } from 'react-i18next'
 
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import Backend from 'i18next-http-backend'
+// import Backend from 'i18next-http-backend'
 import { LanguageCode } from 'meta/languages'
 
 import resources from './resources/resources'
@@ -11,7 +11,7 @@ i18n
 	// load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
 	// learn more: https://github.com/i18next/i18next-http-backend
 	// want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-	.use(Backend)
+	// .use(Backend)
 	// detect user language
 	// learn more: https://github.com/i18next/i18next-browser-languageDetector
 	.use(new LanguageDetector(undefined, { lookupLocalStorage: 'i18nLang' }))
