6dc284ea4357a27b08b68702d4566bf6d4b002ee
NightHarbor <nightharbor91@gmail.com>
2025-08-04 14:57:35 +0200

    POLYGON: NETP Token Integration (#194)

--- utils/contracts/index.ts
+++ utils/contracts/index.ts
@@ -3,12 +3,14 @@ import { oprs } from './oprs'
 import { Address } from 'viem'
 import { dsc } from './dsc'
 import { dgc } from './dgc'
+import { netp } from './netp'
 
 const coingeckoPlatforms: Record<Address, string> = {
 	'0x90685e300a4c4532efcefe91202dfe1dfd572f47': 'ethereum', // CTA
 	'0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': 'ethereum', // WETH
 	'0xf3773fa33A89ec29060C3850583309E6737C007A': 'polygon', // Denario Silver Coin Polygon
 	'0xB2BF2689db4ff1e392d95562c3E71dAAF2d1Bc5F': 'polygon', // Operal wOPRS Polygon
+	'0x01a831D65BD1A1924191656e6EEff6F41EF07091': 'polygon', // NETP Polygon
 }
 
 const toBridgedContract: Record<Address, Address> = {
@@ -21,8 +23,9 @@ const Constants = {
 }
 
 const Prices = {
-	dsc,
 	dgc,
+	dsc,
+	netp,
 	oprs,
 	swissDLT,
 }
